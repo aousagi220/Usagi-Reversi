2e726b6e9c3c0ad6889679a5e77dbbb455c7a7f4
aousagi220 <akausagi.220@gmail.com>
2026-04-02 16:03:55 +0900

    UIのカウントが機能するように変更

--- JS/script.js
+++ JS/script.js
@@ -12,7 +12,7 @@ const board = Array(BOARD_SIZE)
 function resetGame() {
   currentPlayer = BLACK;
   boardReset(board, BOARD_SIZE);
-  renderBoard(board, currentPlayer, BOARD_SIZE);
+  gameUI(board, currentPlayer, BOARD_SIZE, countStone(board, BOARD_SIZE));
 }
 
 function gameEnd() {
@@ -36,7 +36,7 @@ document.getElementById("board").addEventListener("click", (e) => {
 
   if (placeStone(board, x, y, currentPlayer, BOARD_SIZE)) {
     currentPlayer = currentPlayer === BLACK ? WHITE : BLACK;
-    renderBoard(board, currentPlayer, BOARD_SIZE);
+    gameUI(board, currentPlayer, BOARD_SIZE, countStone(board, BOARD_SIZE));
     if (!hasValidMove(board, currentPlayer, BOARD_SIZE)) {
       if (isGameEnd(board, BOARD_SIZE)) {
         console.log("双方置けなくなりました！");
@@ -44,7 +44,7 @@ document.getElementById("board").addEventListener("click", (e) => {
         return;
       } else {
         currentPlayer = currentPlayer === BLACK ? WHITE : BLACK;
-        renderBoard(board, currentPlayer, BOARD_SIZE);
+        gameUI(board, currentPlayer, BOARD_SIZE, countStone(board, BOARD_SIZE));
         console.log("パスされました！");
       }
     }
