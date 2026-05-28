a2ccbdea5c660680aeb9f572039ae9f5911a972e
aousagi220 <akausagi.220@gmail.com>
2026-05-28 11:18:38 +0900

    CPU普通の追加

--- JS/functionCpuFlow.js
+++ JS/functionCpuFlow.js
@@ -7,12 +7,31 @@ function playCpuMove(cpuType) {
   if (cpuType === WEAK) {
     return WeakestMove(validMoves);
   }
+
+  if (cpuType === NORMAL) {
+    return NormalMove(validMoves);
+  }
 }
 
 function WeakestMove(validMoves) {
   // ランダムに有効な手を選択
   const randomIndex = Math.floor(Math.random() * validMoves.length);
   const [x, y] = validMoves[randomIndex];
+  placeStone(x, y, currentPlayer);
+}
+
+function NormalMove(validMoves) {
+  let cpuflippableCount = [];
+
+  for (const [x, y] of validMoves) {
+    const flippableCount = countFlippableStones(x, y, currentPlayer);
+    cpuflippableCount.push({ x, y, flippableCount });
+  }
+
+  const maxFlippable = Math.max(...cpuflippableCount.map((move) => move.flippableCount));
+  const bestMoves = cpuflippableCount.filter((move) => move.flippableCount === maxFlippable);
+  const rundomIndex = Math.floor(Math.random() * bestMoves.length);
+  const { x, y } = bestMoves[rundomIndex];
   placeStone(x, y, currentPlayer);
 }
 
